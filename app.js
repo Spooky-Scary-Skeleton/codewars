03580293952dc8d9387fe3beceba334c08fcacbd
Dan Kim <ddad9583@gmail.com>
2022-01-03 17:11:24 +0900

    feat: implement dotenv

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const configResult = require('dotenv').config();
+
+if (configResult.error) {
+  throw configResult.error;
+}
 
 const index = require("./routes/index");
 const login = require("./routes/login");
